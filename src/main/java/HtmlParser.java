165910d24132d80b7cb6cc3e1645e4a62107a59c
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2021-05-23 17:17:46 +0300

    add words counter

--- src/main/java/HtmlParser.java
+++ src/main/java/HtmlParser.java
@@ -1,3 +1,4 @@
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,6 +8,18 @@ import java.io.IOException;
 import java.util.*;
 
 public class HtmlParser {
+
+    public static int findRecord(ArrayList<Record> records, String word)
+    {
+        int index = 0;
+        for (Record record : records)
+        {
+            if (record.equals(word))
+                return index;
+            index++;
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
 
@@ -44,33 +57,36 @@ public class HtmlParser {
                 + "<title>JSoup Example</title>"
                 + "</head>"
                 + "<body>"
-                + "<table><tr><td><h1>HelloWorld</h1></tr>"
+                + "<table><tr><td><h1>HelloWorld text text</h1></tr>"
                 + "</table>"
                 + "<p>some text idk</p>"
                 + "<div><p>more text</p>"
-                + "<div><p>and more</p></div>"
+                + "<div><p>and more and more</p></div>"
                 + "</div>"
                 + "<a href=\"URL\">текст ссылки</a>"
                 + "<button name=\"nada\">button</button>"
                 + "</body>"
                 + "</html>";
 
-
+        ArrayList<Record> records = new ArrayList<Record>();
 
         Document htmlDocument = Jsoup.parse(HTMLString);
         Elements elements = htmlDocument.getAllElements();
         String[] words;
         for (Element element : elements)
         {
-            if (element.childNodeSize() == 1 && !element.siblingElements().isEmpty())
-            {
+            if (element.childNodeSize() == 1 && !element.siblingElements().isEmpty()) {
                 System.out.println(element.text());
                 words = element.text().split(splitPattern);
-                for (String word : words)
-                    System.out.print(word + "+");
-                System.out.println("\n");
+                for (String word : words) {
+                    if (findRecord(records, word) == 0)
+                        records.add(new Record(word));
+                    else
+                        records.get(findRecord(records, word)).incrementCounter();
+                }
             }
         }
-
+        for (Record record : records)
+            System.out.println(record);
     }
 }
--- src/main/java/Record.java
+++ src/main/java/Record.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Record {
+
+    public String word;
+    public int counter;
+
+    public Record(String word) {
+        this.word = word;
+        this.counter = 1;
+    }
+
+    public void incrementCounter() {
+        this.counter++;
+    }
+
+    public boolean equals(String newWord) {
+        if (this.word.equals(newWord))
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "word='" + word + '\'' +
+                ", counter=" + counter +
+                '}';
+    }
 }
+
