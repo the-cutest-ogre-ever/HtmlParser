b2ab63e9a6bf3fde89ef818a3ae18c109aba8bac
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2021-06-09 23:46:55 +0300

    split to methods

--- src/main/java/HtmlParser.java
+++ src/main/java/HtmlParser.java
@@ -7,18 +7,8 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import java.sql.*;
 
 public class HtmlParser {
-
-    private static final String JDBC_DRIVER = "org.postgresql.Driver";
-    private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/parser";
-
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "shrekislove";
-
-    private static Connection connection;
-    private static Statement statement;
 
     private static Logger LOGGER;
     private static Scanner SCANNER;
@@ -31,9 +21,7 @@ public class HtmlParser {
         } catch (IOException exception) {
             exception.printStackTrace();
         }
-    }
 
-    static {
         SCANNER = new Scanner(System.in);
     }
 
@@ -76,104 +64,13 @@ public class HtmlParser {
         return splitPattern;
     }
 
-    private static void connectDatabase() {
-        try {
-            Class.forName("org.postgresql.Driver");
-
-            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-            statement = connection.createStatement();
-        }
-        catch (ClassNotFoundException exception) {
-            LOGGER.log(Level.WARNING, "ClassNotFoundException occurred", exception);
-        }
-        catch (SQLException exception) {
-            LOGGER.log(Level.WARNING, "SQLException occurred", exception);
-        }
-        catch (Exception exception) {
-            LOGGER.log(Level.WARNING, "Exception occurred", exception);
-        }
-    }
-
-    private static void setUpDataTable() {
-        connectDatabase();
-
-        String sqlRequest = "CREATE TABLE records (" +
-                "               word VARCHAR(100) NOT NULL," +
-                "               counter INT NOT NULL," +
-                "               PRIMARY KEY (word)" +
-                "            );";
-        try  {
-            statement.execute(sqlRequest);
-        }
-        catch (SQLException exception) {
-            LOGGER.log(Level.WARNING, "SQLException occurred", exception);
-        }
-        catch (Exception exception) {
-            LOGGER.log(Level.WARNING, "Exception occurred", exception);
-        }
-    }
-
-    private static void closeDatabaseConnection() {
-        String sqlRequest = "DROP TABLE records;";
-        try  {
-            statement.execute(sqlRequest);
-
-            statement.close();
-            connection.close();
-        }
-        catch (SQLException exception) {
-            LOGGER.log(Level.WARNING, "SQLException occurred", exception);
-        }
-        catch (Exception exception) {
-            LOGGER.log(Level.WARNING, "Exception occurred", exception);
-        }
-    }
-
-    private static void doSomeStuff() {
-        ResultSet resultSet;
-        String sql1 = "INSERT INTO records (word, counter) VALUES ('word', '100');";
-        String sql2 = "INSERT INTO records (word, counter) VALUES ('word2', '1002');";
-        String sql3 = "SELECT * FROM records";
-
-        try {
-            resultSet = statement.executeQuery(sql1);
-            resultSet = statement.executeQuery(sql2);
-            resultSet = statement.executeQuery(sql3);
-
-            System.out.println("SQL RESULT:");
-            while (resultSet.next()) {
-                String word = resultSet.getString("word");
-                int counter = resultSet.getInt("counter");
-
-                System.out.println(word + " " + counter);
-            }
-
-            resultSet.close();
-        }
-        catch (SQLException exception) {
-            LOGGER.log(Level.WARNING, "SQLException occurred", exception);
-        }
-        catch (Exception exception) {
-            LOGGER.log(Level.WARNING, "Exception occurred", exception);
-        }
-
-    }
-
-    public static void main(String[] args) throws ClassNotFoundException, SQLException{
-
-        setUpDataTable();
-        doSomeStuff();
-        closeDatabaseConnection();
-
-        String url = getUrl(args);
-
-        Document htmlDocument = null;
+    private static ArrayList<Record> getRecords(String url) {
         String splitPattern = getSplitPattern();
+        String[] words;
+        ArrayList<Record> records = new ArrayList<Record>();
 
         try {
-            ArrayList<Record> records = new ArrayList<Record>();
-            String[] words;
-            htmlDocument = Jsoup.connect(url).userAgent("Chrome/4.0.249.0").get();
+            Document htmlDocument = Jsoup.connect(url).userAgent("Chrome/4.0.249.0").get();
             Elements elements = htmlDocument.getAllElements();
 
             for (Element element : elements) {
@@ -188,8 +85,6 @@ public class HtmlParser {
                     }
                 }
             }
-            for (Record record : records)
-                System.out.println(record);
         }
         catch (IOException exception) {
             LOGGER.log(Level.WARNING, "IOException occurred", exception);
@@ -197,5 +92,14 @@ public class HtmlParser {
         catch (Exception exception) {
             LOGGER.log(Level.WARNING, "Exception occurred", exception);
         }
+        return records;
+    }
+
+    public static void main(String[] args) {
+        String url = getUrl(args);
+        ArrayList<Record> records = getRecords(url);
+
+        for (Record record : records)
+            System.out.println(record);
     }
 }
